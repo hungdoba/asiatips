33c0ea373681fe021c465a836d930bf8c508e8ce
hungdoba <hungdoba.hdb@gmail.com>
2023-11-26 16:29:46 +0900

    update .gitignore file

--- components/article/underline/green.tsx
+++
@@ -1,7 +0,0 @@
-export default function UnderGreen({ content }) {
-  return (
-    <span className="inline-block underline decoration-green-300 decoration-2">
-      {content}
-    </span>
-  );
-}
--- components/article/underline/yellow.tsx
+++
@@ -1,7 +0,0 @@
-export default function UnderYellow({ content }) {
-  return (
-    <span className="inline-block underline decoration-yellow-300 decoration-2">
-      {content}
-    </span>
-  );
-}
